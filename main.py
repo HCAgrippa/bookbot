1913bdfcedbfbbfabf7a13e839124a964dd9e920
chris <kreklewetz@gmail.com>
2025-04-25 11:35:30 -0400

    main.py outputs frankenstein.txt

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(bookfile):
+    with open(bookfile) as f:
+        book_contents = f.read()
+        return book_contents
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
