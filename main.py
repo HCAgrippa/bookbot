e4276dfc9a88370a417ca098e57bfd36e241a12c
chris <kreklewetz@gmail.com>
2025-04-27 15:52:21 -0400

    some changes

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-from stats import get_word_num, get_char_count, get_sorted_char_list
-
+from stats import (
+    get_word_num,
+    get_char_count,
+    get_sorted_char_list
+)
 
 def main():
     book = get_book_text("books/frankenstein.txt")
     words = get_word_num(book)
     char_dic = get_char_count(book)
     sorted_char_list = get_sorted_char_list(char_dic)
-    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\nFound {words} total words\n--------- Character Count -------")
-    for dic in sorted_char_list:
-        char = dic["char"]
-        num = dic["num"]
-        print(f"{char}: {num}")
-    print("============= END ===============")
+    report(book, words, sorted_char_list)
 
 def get_book_text(bookfile):
     with open(bookfile) as f:
         book = f.read()
         return book
 
+def report(book, words, sorted_char_list):
+    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\nFound {words} total words\n--------- Character Count -------")
+    for dic in sorted_char_list:
+        char = dic["char"]
+        num = dic["num"]
+        print(f"{char}: {num}")
+    print("============= END ===============")
 
 main()
