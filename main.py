fe30a296216ea3496a50853601a3eacbff6a7dbc
chris <kreklewetz@gmail.com>
2025-04-26 20:10:41 -0400

    sorts list of characters

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from stats import get_word_num
-from stats import get_char_count
+from stats import get_word_num, get_char_count, get_sorted_char_list
+
 
 def main():
     book = get_book_text("books/frankenstein.txt")
     words = get_word_num(book)
-    print(f"{words} words found in the document")
     char_dic = get_char_count(book)
-    print(char_dic)
-#    for char in char_dic:
-#        print(f"{char}: {char_dic[char]}")
+    sorted_char_list = get_sorted_char_list(char_dic)
+    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\nFound {words} total words\n--------- Character Count -------")
+    for dic in sorted_char_list:
+        char = dic["char"]
+        num = dic["num"]
+        print(f"{char}: {num}")
+    print("============= END ===============")
 
 def get_book_text(bookfile):
     with open(bookfile) as f:
         book = f.read()
         return book
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,13 +1,22 @@
 def get_word_num(book):
-     return len(book.split())
+    return len(book.split())
 
 def get_char_count(book):
-     lc_book = book.lower()
-     char_dic = {}
-     for char in lc_book:
-         if char in char_dic:
-             char_dic[char] += 1
-         else:
-             char_dic[char] = 1
-     return char_dic
+    lc_book = book.lower()
+    char_dic = {}
+    for char in lc_book:
+        if char in char_dic:
+            char_dic[char] += 1
+        else:
+            char_dic[char] = 1
+    return char_dic
 
+def get_sorted_char_list(char_dic):
+    sorted_char_list = []
+    for char in char_dic:
+        if char.isalpha and char != " " and char != "\n":
+            sorted_char_list.append({"char": char, "num": char_dic[char]})
+    def sort_key(dic):
+        return dic["num"]
+    sorted_char_list.sort(reverse=True, key=sort_key)
+    return sorted_char_list
